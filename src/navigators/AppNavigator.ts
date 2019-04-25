123fa21999861ceaf6ccee9fb4bd41b8434cf09d
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-04-25 13:56:28 +0800

    Fix AppNavigator.ts

--- src/navigators/AppNavigator.ts
+++ src/navigators/AppNavigator.ts
@@ -1,16 +1,12 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import AuthNavigator from './AuthNavigator';
 import MainNavigator from './MainNavigator';
-import ConnectScreen from '../screens/ConnectScreen';
 
 const AppNavigator = createSwitchNavigator(
   {
     Main: MainNavigator,
-    Auth: AuthNavigator,
-    Connect: ConnectScreen,
   },
   {
-    initialRouteName: 'Auth',
+    initialRouteName: 'Main',
   },
 );
 
