9e4bcd1d47259d3a4a0b2b9f6039f7c1a1fc8667
TseHang <a54383813@gmail.com>
2019-06-05 19:26:12 +0800

    add setDefaultPersistOptions

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { setPersistStorageDefaults } from 'react-native-use-persist-storage';
+
 import AppNavigator from '@/navigators/AppNavigator';
 import AppContextsProvider from '@/contexts/AppContextsProvider';
+
+setPersistStorageDefaults({ debug: true });
 
 export default () => (
   <AppContextsProvider>
